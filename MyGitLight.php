e84ccbabb3ad94627427facd76fd922b6578531b
ERICAntoine <antoine.micro@gmail.com>
2018-10-20 09:08:19 +0200

    push

--- MyGitLight.php
+++ MyGitLight.php
@@ -1,17 +1,30 @@
 <?php
+
+    $help = "démarrer une zone de travail : \n
+    init       Créer un dépôt Git vide ou réinitialiser un existant";
     function myGitLight()
     {
-        global $argc, $argv;
-        $directory = mkdir(".MyGitLight");
+        global $argc, $argv, $help;
         $file_name = "MyGitLight";
+        $argv[1] = NULL;
 
         $content = file_get_contents($argv[0]);
 
-        if($argv[0] == "MyGitLight.php")
+        if($argv[1] == "init")
         {
-            file_put_contents($file_name, $content);
+            if($argv[0] == "MyGitLight.php")
+            {
+                file_put_contents($file_name, $content);
+                echo "Depot GitLight initialisé !";
+            }
         }
-        rename("MyGitLight", "/.MyGitLight");
+        else
+        {
+            echo $help;
+            return 0;
+        }
+        $directory = mkdir(".MyGitLight");
+        rename($file_name, ".MyGitLight/".$file_name);
     }
 
     myGitLight();
